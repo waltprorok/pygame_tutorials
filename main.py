8ebf51f48d87efb1fc583902e2516800d76ac2bd
Walter Prorok <w.prorok@ubreakifix.com>
2020-07-19 17:40:17 -0400

    added collision detection on characters

--- main.py
+++ main.py
@@ -78,6 +78,23 @@ class Player(object):
         self.hitbox = (self.x + 17, self.y + 11, 29, 52)
         # pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
+    def hit(self):
+        self.x = 60
+        self.y = 410
+        self.walkCount = 0
+        font1 = pygame.font.SysFont('comicsans', 100)
+        text = font1.render('-5', 1, (255, 0, 0))
+        win.blit(text, (250 - (text.get_width() / 2), 200))
+        pygame.display.update()
+        i = 0
+        while i < 300:
+            pygame.time.delay(10)
+            i += 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    i = 301
+                    pygame.quit()
+
 
 class Projectile(object):
     def __init__(self, x, y, radius, color, facing):
@@ -190,6 +207,11 @@ run = True
 
 while run:
     clock.tick(27)
+
+    if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
+        if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
+            man.hit()
+            score -= 5
 
     if shootLoop > 0:
         shootLoop += 1
